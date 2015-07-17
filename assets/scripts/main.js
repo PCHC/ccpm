329bd5fa9ea2cc3f072ead6c3fe96d1ba0ba8426
Chris Violette <cvioletterun@gmail.com>
2015-07-17 13:52:56 -0400

    Updated gulpfile to not completely fail on JSHint fail

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -74,6 +74,46 @@
     }
   };
 
+  function LightenDarkenColor(col, amt) {
+
+    var usePound = false;
+
+    if (col[0] === "#") {
+      col = col.slice(1);
+      usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if (r > 255) {
+      r = 255;
+    } else if (r < 0) {
+      r = 0;
+    }
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if (b > 255) {
+      b = 255;
+    } else if (b < 0) {
+      b = 0;
+    }
+
+    var g = (num & 0x0000FF) + amt;
+
+    if (g > 255) {
+      g = 255;
+    } else if (g < 0) {
+      g = 0;
+    }
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+
+  }
+
+
   /*
   *  render_map
   *
@@ -204,7 +244,7 @@
   	});
 
   	// only 1 marker?
-  	if( map.markers.length == 1 )
+  	if( map.markers.length === 1 )
   	{
   		// set center of map
   	    map.setCenter( bounds.getCenter() );
@@ -222,33 +262,3 @@
   $(document).ready(UTIL.loadEvents);
 
 })(jQuery); // Fully reference jQuery after this point.
-
-function LightenDarkenColor(col, amt) {
-
-  var usePound = false;
-
-  if (col[0] == "#") {
-    col = col.slice(1);
-    usePound = true;
-  }
-
-  var num = parseInt(col,16);
-
-  var r = (num >> 16) + amt;
-
-  if (r > 255) r = 255;
-  else if  (r < 0) r = 0;
-
-  var b = ((num >> 8) & 0x00FF) + amt;
-
-  if (b > 255) b = 255;
-  else if  (b < 0) b = 0;
-
-  var g = (num & 0x0000FF) + amt;
-
-  if (g > 255) g = 255;
-  else if (g < 0) g = 0;
-
-  return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
-
-}

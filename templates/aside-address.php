7d0f0d2afffae4faa6e399f5b474419b269b5c96
Chris Violette <cvioletterun@gmail.com>
2015-07-20 15:44:27 -0400

    Fixed output of phone & website fields.

--- templates/aside-address.php
+++ templates/aside-address.php
@@ -1,10 +1,10 @@
 <?php $address = get_field('address'); if(!empty($address)) : ?>
   <address>
     <?php echo $address['address']; ?>
-    <?php if( !empty( get_field( 'phone' ) ) ) {
+    <?php if( get_field( 'phone' ) ) {
       echo '<br><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> ' . get_field('phone');
     } ?>
-    <?php if( !empty( get_field( 'website' ) ) ) {
+    <?php if( get_field( 'website' ) ) {
       echo '<br><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> <a target="_blank" href="http://'.get_field('website').'">' . get_field('website') . '</a>';
     } ?>
   </address>

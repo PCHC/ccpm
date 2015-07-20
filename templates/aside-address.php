8ebbcadc68d4014a1c3833c1a656dc03780f7f8e
Chris Violette <cvioletterun@gmail.com>
2015-07-20 14:56:36 -0400

    Updated output of addresses, phone

--- templates/aside-address.php
+++ templates/aside-address.php
@@ -1,7 +1,11 @@
 <?php $address = get_field('address'); if(!empty($address)) : ?>
   <address>
     <?php echo $address['address']; ?>
-    <?php echo !empty( get_field( 'phone' ) ) ? '<br><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> ' . get_field('phone') : ''; ?>
-    <?php echo !empty( get_field( 'website' ) ) ? '<br><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> <a target="_blank" href="http://'.get_field('website').'">' . get_field('website') . '</a>' : ''; ?>
+    <?php if( !empty( get_field( 'phone' ) ) ) {
+      echo '<br><span class="glyphicon glyphicon-phone" aria-hidden="true"></span> ' . get_field('phone');
+    } ?>
+    <?php if( !empty( get_field( 'website' ) ) ) {
+      echo '<br><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> <a target="_blank" href="http://'.get_field('website').'">' . get_field('website') . '</a>';
+    } ?>
   </address>
 <?php endif; ?>

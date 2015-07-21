7a01d79e5ed69d118aae5da2feaef7284b6ea0ac
Chris Violette <cvioletterun@gmail.com>
2015-07-21 08:25:23 -0400

    Adjusted layout of pages.

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
       <?php get_template_part('templates/page', 'header'); ?>
       <div class="row">
-        <div class="col-xs-10 col-sm-6 col-xs-offset-1 <?php echo ( has_post_thumbnail() ) ? 'col-sm-offset-1' : 'col-sm-offset-3'; ?>">
+        <div class="col-xs-10 col-xs-offset-1 <?php echo ( has_post_thumbnail() ) ? 'col-sm-6 col-sm-offset-1' : 'col-sm-8'; ?>">
           <?php get_template_part('templates/content', 'page'); ?>
         </div>
         <?php if( has_post_thumbnail() ) : ?>

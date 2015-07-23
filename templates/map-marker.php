4010b6b7ea3ec306e4400f0608612b46a4cef5a6
Chris Violette <cvioletterun@gmail.com>
2015-07-23 11:55:52 -0400

    Changed display of map markers to include link to parent site.

--- templates/map-marker.php
+++ templates/map-marker.php
@@ -3,7 +3,13 @@
   if( !empty( $address['lat'] ) && !empty( $address['lng'] ) ) : ?>
 
 <div class="marker" data-lat="<?php echo $address['lat']; ?>" data-lng="<?php echo $address['lng']; ?>" data-pincolor="<?php echo $pincolor; ?>">
-  <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+  <?php $member_parent = $post->post_parent;
+  if( $member_parent != 0 ) : ?>
+    <h5><a href="<?php echo get_the_permalink( $member_parent ); ?>"><?php echo get_the_title( $member_parent ); ?></a></h5>
+    <p><strong><?php the_title(); ?></strong></p>
+  <?php else : ?>
+    <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+  <?php endif; ?>
   <p><?php echo $address['address']; ?></p>
 </div>
 

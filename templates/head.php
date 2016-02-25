f3126225dca4ce4583fc67330bc225ec8e3bc28e
Chris Violette <cvioletterun@gmail.com>
2016-02-25 11:49:33 -0500

    Added favicons

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico" />
+  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.png" />
   <?php wp_head(); ?>
 </head>

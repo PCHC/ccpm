cf3519d051713ad5d536688389f55cea29b98933
Chris Violette <cvioletterun@gmail.com>
2016-02-25 12:12:12 -0500

    Added theme colors for mobile address bar

--- templates/head.php
+++ templates/head.php
@@ -4,5 +4,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.ico" />
   <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/dist/images/favicon.png" />
+  <meta name="theme-color" content="#005F84">
+  <meta name="msapplication-navbutton-color" content="#005F84">
+  <meta name="apple-mobile-web-app-status-bar-style" content="#005F84">
   <?php wp_head(); ?>
 </head>

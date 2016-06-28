71557285b85a54f4bf9d507045ac0dc55c6c3804
Chris Violette <cvioletterun@gmail.com>
2016-06-28 11:24:11 -0400

    Fixed display of single post

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,12 +1,15 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <div class="row">
-      <div class="col-xs-3 col-sm-4">
-        <?php the_post_thumbnail(); ?>
-      </div>
-      <div class="col-xs-9 col-sm-8">
+      <?php if( has_post_thumbnail() ) : ?>
+        <div class="col-xs-3 col-sm-4">
+          <?php the_post_thumbnail(); ?>
+        </div>
+      <?php endif; ?>
+      <div class="<?php echo ( has_post_thumbnail() ) ? 'col-xs-9 col-sm-8' : 'col-xs-12'; ?>">
         <header>
           <h1 class="entry-title"><?php the_title(); ?></h1>
+          <?php get_template_part('templates/entry-meta'); ?>
         </header>
         <div class="entry-content">
           <?php the_content(); ?>

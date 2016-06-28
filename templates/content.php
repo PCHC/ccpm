27cd6f6e301cb8c49f157e5797c9c8f70f62b05f
Chris Violette <cvioletterun@gmail.com>
2016-06-28 11:40:07 -0400

    Show full content on news archive page.

--- templates/content.php
+++ templates/content.php
@@ -12,7 +12,7 @@
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-summary">
-      <?php the_excerpt(); ?>
+      <?php the_content(); ?>
     </div>
   </div>
 </article>

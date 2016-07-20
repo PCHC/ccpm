345adb474a1982608cc1be0e2e36b202872359ea
Chris Violette <cvioletterun@gmail.com>
2016-07-20 15:22:45 -0400

    Added template for full-width pages.

--- template-custom.php
+++ template-custom.php
@@ -1,10 +1,20 @@
 <?php
 /**
- * Template Name: Custom Template
+ * Template Name: Full-Width Page
  */
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+      <?php get_template_part('templates/page', 'header'); ?>
+      <div class="row">
+        <div class="col-xs-12">
+          <?php if( has_post_thumbnail() ) : ?>
+            <div class="pull-right col-xs-4">
+              <?php the_post_thumbnail(); ?>
+            </div>
+          <?php endif; ?>
+          <?php get_template_part('templates/content', 'page'); ?>
+        </div>
+      </div>
+  </div>
 <?php endwhile; ?>
